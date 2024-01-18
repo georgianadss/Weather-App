e183e856f8b5c99db15f9ce3e3944c4a3dc2ef11
Georgiana <ogeorgiana.dss@gmail.com>
2024-01-18 17:28:19 +0200

    Created scss global files

--- weather-app/src/app/modules/app/app.routes.ts
+++ weather-app/src/app/modules/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HeaderComponent,
+        pathMatch: 'full',
+        
+    }
+];
--- weather-app/src/app/modules/app/components/header/header.component.ts
+++ weather-app/src/app/modules/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
+  standalone: true,
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
 
