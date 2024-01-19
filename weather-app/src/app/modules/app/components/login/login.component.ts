2b5b2037ec1b97de1b8da6e6c0ff15047014ea25
Georgiana <ogeorgiana.dss@gmail.com>
2024-01-19 16:45:23 +0200

    Created FormGroup with FormControls

--- weather-app/src/app/modules/app/components/login/login.component.ts
+++ weather-app/src/app/modules/app/components/login/login.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  imports: [ FormsModule, ReactiveFormsModule ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public formGroup!: FormGroup;
 
+  get userNameControl() {
+    return this.formGroup.get('userNameControl') as FormControl;
+  }
+
+  get passwordControl() {
+    return this.formGroup.get('passwordControl') as FormControl;
+  }
+
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      userNameControl: new FormControl(),
+      passwordControl: new FormControl() 
+    })
+  }
+
+  onSubmit() {
+    console.log(this.formGroup.value);
+  }
 }
