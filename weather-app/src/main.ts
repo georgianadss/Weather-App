f70b370270467d900d1efe20c9d8dcb5b8dcff09
Georgiana <ogeorgiana.dss@gmail.com>
2024-01-31 01:15:48 +0200

    Removes unused import.

--- weather-app/src/main.ts
+++ weather-app/src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/modules/app/app.config';
 import { AppComponent } from './app/modules/app/app.component';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTE } from './app/modules/app/app.routes';
