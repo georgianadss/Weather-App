79b4692a57fc733a263cffd3d6d051bdf40df2be
Georgiana <ogeorgiana.dss@gmail.com>
2024-01-31 18:46:55 +0200

    Created Current-Weather Component

---
+++ weather-app/src/app/modules/app/components/current-weather/current-weather.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-current-weather',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.scss']
+})
+export class CurrentWeatherComponent {
+
+}
--- weather-app/src/app/modules/app/home/home.component.ts
+++ weather-app/src/app/modules/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
+import { CurrentWeatherComponent } from '../components/current-weather/current-weather.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent, CurrentWeatherComponent,],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
