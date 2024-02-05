7f3add9005cd50ee7d928733d7d8739fe89d6f59
Georgiana <ogeorgiana.dss@gmail.com>
2024-02-05 18:11:39 +0200

    Get Country List and Displayed in HTML Template

--- weather-app/src/app/modules/app/components/current-weather/current-weather.component.ts
+++ weather-app/src/app/modules/app/components/current-weather/current-weather.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Region, LocationsService, TopCityList } from '../../../../services/locations.service';
+
+const countryId = 'AFR';
 
 @Component({
   selector: 'app-current-weather',
@@ -8,6 +11,27 @@ import { CommonModule } from '@angular/common';
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.scss']
 })
-export class CurrentWeatherComponent {
+export class CurrentWeatherComponent implements OnInit {
+  public regions: Region[] = [];
 
+  public topCitiesList: TopCityList[] = [];
+
+  constructor(private locationService: LocationsService) { }
+
+  ngOnInit(): void {
+    // this.locationService.getRegionCode().subscribe((regions) => {
+    //   if (!regions) return;
+    //   this.regions = regions;
+    //   console.log(regions)
+    // }),
+
+    //   this.locationService.getCountryList(countryId).subscribe((b) => {
+    //     console.log(b);
+    //   }),
+
+      this.locationService.getTopCityList().subscribe((topCitiesList) => {
+        this.topCitiesList = topCitiesList;
+        console.log(topCitiesList);
+      })
+  }
 }
--- weather-app/src/app/modules/app/home/home.component.ts
+++ weather-app/src/app/modules/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
 import { CurrentWeatherComponent } from '../components/current-weather/current-weather.component';
+import {  HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent, CurrentWeatherComponent,],
+  imports: [CommonModule, RouterModule, HeaderComponent, CurrentWeatherComponent, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
