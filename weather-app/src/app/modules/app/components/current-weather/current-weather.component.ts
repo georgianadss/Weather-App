9ae7923214c547ec3c6f54bda0a88eb85705acc0
Georgiana <ogeorgiana.dss@gmail.com>
2024-02-05 18:24:29 +0200

    Resolved as comments reviews

--- weather-app/src/app/modules/app/components/current-weather/current-weather.component.ts
+++ weather-app/src/app/modules/app/components/current-weather/current-weather.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Region, LocationsService, TopCityList } from '../../../../services/locations.service';
+import { LocationsService, TopCityList } from '../../../../services/locations.service';
 
 const countryId = 'AFR';
 
@@ -12,26 +12,14 @@ const countryId = 'AFR';
   styleUrls: ['./current-weather.component.scss']
 })
 export class CurrentWeatherComponent implements OnInit {
-  public regions: Region[] = [];
-
   public topCitiesList: TopCityList[] = [];
 
   constructor(private locationService: LocationsService) { }
 
   ngOnInit(): void {
-    // this.locationService.getRegionCode().subscribe((regions) => {
-    //   if (!regions) return;
-    //   this.regions = regions;
-    //   console.log(regions)
-    // }),
-
-    //   this.locationService.getCountryList(countryId).subscribe((b) => {
-    //     console.log(b);
-    //   }),
-
-      this.locationService.getTopCityList().subscribe((topCitiesList) => {
-        this.topCitiesList = topCitiesList;
-        console.log(topCitiesList);
-      })
+    this.locationService.getTopCityList().subscribe((topCitiesList) => {
+      this.topCitiesList = topCitiesList;
+      console.log(topCitiesList);
+    })
   }
 }
