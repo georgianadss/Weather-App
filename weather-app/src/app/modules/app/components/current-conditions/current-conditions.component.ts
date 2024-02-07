feee8a2ea98636203bd99c92376414dd1ddf7897
Georgiana <ogeorgiana.dss@gmail.com>
2024-02-07 13:00:37 +0200

    Created Current-Conditions Component and Models Folders with Current-Conditions ts file with current-condition interface model

---
+++ weather-app/src/app/modules/app/components/current-conditions/current-conditions.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-current-conditions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './current-conditions.component.html',
+  styleUrls: ['./current-conditions.component.scss']
+})
+export class CurrentConditionsComponent {
+
+}
