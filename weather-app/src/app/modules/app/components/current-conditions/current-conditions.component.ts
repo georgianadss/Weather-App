68b43272135838c93576e026c9cff2c2d6fd6e21
Georgiana <ogeorgiana.dss@gmail.com>
2024-02-07 18:40:58 +0200

    Created functionality for dropdown

--- weather-app/src/app/modules/app/components/current-conditions/current-conditions.component.ts
+++ weather-app/src/app/modules/app/components/current-conditions/current-conditions.component.ts
@@ -14,22 +14,28 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 export class CurrentConditionsComponent implements OnInit{
   public currentConditions!: CurrentCondition[];
   public topCities!: TopCityList[];
+  public cityName!: string;
   // TODO: remove this after we create the dropdown 
  readonly ID: number = 264120;
 
- constructor(private locationService: LocationsService) {}
+ constructor(private locationService: LocationsService) {
+  console.log('consturctor');
+ }
 
  ngOnInit(): void {
-   this.locationService.getCurrentCondition(this.ID).subscribe((currentConditions) => {
-    this.currentConditions = currentConditions;
-   })
+  console.log('ngonit');
    this.locationService.getTopCityList().subscribe((topCities) => {
+    if(!topCities) return;
     this.topCities = topCities;
    })   
  }
 
  
- selectCity(key: number) {
-   console.log(key)
+ selectCity(key: number, cityName: string) {
+  this.cityName = cityName;
+  this.locationService.getCurrentCondition(key).subscribe((currentConditions) => {
+    if(!currentConditions) return;
+    this.currentConditions = currentConditions;
+   })
  }
 }
