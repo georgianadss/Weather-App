ab21146490292c1cd7499773883c14d01dba66cd
Georgiana <ogeorgiana.dss@gmail.com>
2024-02-07 17:54:49 +0200

    Implemented ng Dropdown

--- weather-app/src/app/modules/app/components/current-conditions/current-conditions.component.ts
+++ weather-app/src/app/modules/app/components/current-conditions/current-conditions.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LocationsService } from '../../../../services/locations.service';
+import { LocationsService, TopCityList } from '../../../../services/locations.service';
 import { CurrentCondition } from '../../../models/current-conditions';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-current-conditions',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CommonModule, NgbDropdownModule],
   templateUrl: './current-conditions.component.html',
   styleUrls: ['./current-conditions.component.scss']
 })
 export class CurrentConditionsComponent implements OnInit{
   public currentConditions!: CurrentCondition[];
+  public topCities!: TopCityList[];
   // TODO: remove this after we create the dropdown 
  readonly ID: number = 264120;
 
@@ -20,7 +22,14 @@ export class CurrentConditionsComponent implements OnInit{
  ngOnInit(): void {
    this.locationService.getCurrentCondition(this.ID).subscribe((currentConditions) => {
     this.currentConditions = currentConditions;
-    console.log(currentConditions);
    })
+   this.locationService.getTopCityList().subscribe((topCities) => {
+    this.topCities = topCities;
+   })   
+ }
+
+ 
+ selectCity(key: number) {
+   console.log(key)
  }
 }
