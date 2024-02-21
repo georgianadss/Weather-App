e26b215a99389f666fba7328969a68d57822cb6e
Georgiana <ogeorgiana.dss@gmail.com>
2024-02-21 10:52:26 +0200

    Created a new interface to create a new object when selecting a city

--- weather-app/src/app/modules/app/components/suggestions/suggestions.component.ts
+++ weather-app/src/app/modules/app/components/suggestions/suggestions.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Select } from '@ngxs/store';
 import { AppState } from '../../../state/app.state';
 import { Observable, Subject, takeUntil, tap } from 'rxjs';
-import { CityData, CityDetails } from '../../../models/city-data';
+import { City, CityData, CityDetails } from '../../../models/city-data';
 
 @Component({
   selector: 'app-suggestions',
@@ -23,6 +23,19 @@ export class SuggestionsComponent implements OnInit, OnDestroy {
     this.cities$.pipe(takeUntil(this.unsubscribe)).subscribe((cities) => {
       this.cities = cities;
     })
+  }
+
+  selectedCity(city: CityDetails) {
+    console.log(city);
+    const newCity: City = {
+      key: city.Key,
+      cityName: city.LocalizedName,
+      postalCode: city.PrimaryPostalCode,
+      regionName: city.Region.LocalizedName,
+      countryName: city.Country.LocalizedName,
+      geoPosition: city.GeoPosition.Elevation.Imperial.Value,
+    }
+    console.log(newCity);
   }
 
   ngOnDestroy(): void {
--- weather-app/src/app/modules/models/city-data.ts
+++ weather-app/src/app/modules/models/city-data.ts
@@ -1,79 +1,88 @@
 export interface CityDetails {
-      Key: string,
-      Type: string,
-      Rank: number,
-      LocalizedName: string,
-      PrimaryPostalCode: string,
-      Region: {
-        ID: string,
-        LocalizedName: string,
-        EnglishName: string
+  Key: string,
+  Type: string,
+  Rank: number,
+  LocalizedName: string,
+  PrimaryPostalCode: string,
+  Region: {
+    ID: string,
+    LocalizedName: string,
+    EnglishName: string
+  },
+  Country: {
+    ID: string,
+    LocalizedName: string,
+    EnglishName: string
+  },
+  AdministrativeArea: {
+    ID: string,
+    LocalizedName: string,
+    EnglishName: string,
+    Level: number,
+    LocalizedType: string,
+    EnglishType: string,
+    CountryID: string
+  },
+  TimeZone: {
+    Code: string,
+    Name: string,
+    GmtOffset: number,
+    IsDaylightSaving: boolean,
+    NextOffsetChange: string
+  },
+  GeoPosition: {
+    Latitude: number,
+    Longitude: number,
+    Elevation: {
+      Metric: {
+        Value: number,
+        Unit: string,
+        UnitType: number
       },
-      Country: {
-        ID: string,
-        LocalizedName: string,
-        EnglishName: string
-      },
-      AdministrativeArea: {
-        ID: string,
-        LocalizedName: string,
-        EnglishName: string,
-        Level: number,
-        LocalizedType: string,
-        EnglishType: string,
-        CountryID: string
-      },
-      TimeZone: {
-        Code: string,
-        Name: string,
-        GmtOffset: number,
-        IsDaylightSaving: boolean,
-        NextOffsetChange: string
-      },
-      GeoPosition: {
-        Latitude: number,
-        Longitude: number,
-        Elevation: {
-          Metric: {
-            Value: number,
-            Unit: string,
-            UnitType: number
-          },
-          Imperial: {
-            Value: number,
-            Unit: string,
-            UnitType: number
-          }
-        }
-      },
-      IsAlias: boolean,
-      SupplementalAdminAreas: [
-        {
-          Level: number,
-          LocalizedName: string,
-          EnglishName: string
-        },
-        {
-          Level: number,
-          LocalizedName: string,
-          EnglishName: string
-        }
-      ],
-      DataSets: string[]
-    }
-
-    export interface CityData   {
-        Version: number,
-        Key: string,
-        Type: string,
-        Rank: number,
-        LocalizedName: string,
-        Country: {
-          ID: string,
-          LocalizedName: string
-        },
-        AdministrativeArea: {
-          ID: string,
-          LocalizedName: string
-        }
+      Imperial: {
+        Value: number,
+        Unit: string,
+        UnitType: number
       }
+    }
+  },
+  IsAlias: boolean,
+  SupplementalAdminAreas: [
+    {
+      Level: number,
+      LocalizedName: string,
+      EnglishName: string
+    },
+    {
+      Level: number,
+      LocalizedName: string,
+      EnglishName: string
+    }
+  ],
+  DataSets: string[]
+}
+
+export interface CityData {
+  Version: number,
+  Key: string,
+  Type: string,
+  Rank: number,
+  LocalizedName: string,
+  Country: {
+    ID: string,
+    LocalizedName: string
+  },
+  AdministrativeArea: {
+    ID: string,
+    LocalizedName: string
+  }
+}
+
+export interface City {
+  key: string,
+  cityName: string,
+  postalCode: string,
+  regionName: string,
+  countryName: string,
+  geoPosition: number,
+}
