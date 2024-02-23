7b07995ece97f21fdb2eb04cbbb5d31e6cd1f37e
Georgiana <ogeorgiana.dss@gmail.com>
2024-02-23 16:26:38 +0200

    Get data from the state

--- weather-app/src/app/modules/app/components/favorite-cities/favorite-cities.component.ts
+++ weather-app/src/app/modules/app/components/favorite-cities/favorite-cities.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { Select } from '@ngxs/store';
+import { AppState } from '../../../state/app.state';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { City } from '../../../models/city-data';
 
 @Component({
     selector: 'app-favorite-cities',
@@ -9,6 +13,19 @@ import { HeaderComponent } from '../header/header.component';
     styleUrls: ['./favorite-cities.component.scss'],
     imports: [CommonModule, HeaderComponent]
 })
-export class FavoriteCitiesComponent {
+export class FavoriteCitiesComponent implements OnInit{
+@Select(AppState.favoriteCities) favoriteCities$!: Observable<City[]>
 
+public favoriteCitiesList!: City[];
+
+readonly unsubscribe = new Subject<void>();
+
+constructor() {}
+
+ngOnInit(): void {
+  this.favoriteCities$.pipe(takeUntil(this.unsubscribe)).subscribe((saveCityToFavorites) => {
+    this.favoriteCitiesList = saveCityToFavorites;
+    console.log(saveCityToFavorites);
+  })
+}
 }
